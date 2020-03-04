53c33c81793862e282e1599006e878d7d2da2ca5
bgotchall <bgotchall@gmail.com>
2020-03-04 15:38:02 -0600

    basic table in place.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
+
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Table from './components/Table.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Header id="header" />
+        </Grid>
+        
+        <Grid item xs={12}>
+          <Table />
+        </Grid>
+        <Grid item xs={12}>
+          <Footer />
+        </Grid>
+
+      </Grid>
+
+
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Header() {
+
+    return (
+        <div id="header">
+            <h1 id="header-text">Employee Directory</h1>
+            <p id="header-text">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        </div>
+    )
+}
+
+export default Header;
